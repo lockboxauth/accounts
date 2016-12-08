f02226d1dd1e37af520c634e71c486dd7ae9e205
Paddy <paddy@impractical.co>
2016-12-07 21:58:55 -0800

    Update to use HashiCorp's MemDB.
    As an experiment, use MemDB instead of our roll-your-own in-memory
    database. This seems to work out well, with less application-specific
    code.

--- storers/memory.go
+++ storers/memory.go
@@ -2,36 +2,69 @@ package storers
 
 import (
 	"context"
-	"sync"
+
+	memdb "github.com/hashicorp/go-memdb"
 
 	"code.impractical.co/accounts"
+)
+
+var (
+	schema = &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{
+			"account": &memdb.TableSchema{
+				Name: "account",
+				Indexes: map[string]*memdb.IndexSchema{
+					"id": &memdb.IndexSchema{
+						Name:    "id",
+						Unique:  true,
+						Indexer: &memdb.StringFieldIndex{Field: "ID"},
+					},
+					"profileID": &memdb.IndexSchema{
+						Name:    "profileID",
+						Indexer: &memdb.StringFieldIndex{Field: "ProfileID"},
+					},
+				},
+			},
+		},
+	}
 )
 
 // Memstore is an in-memory implementation of the Storer
 // interface.
 type Memstore struct {
-	accounts map[string]accounts.Account
-	lock     sync.RWMutex
+	db *memdb.MemDB
 }
 
 // NewMemstore returns a Memstore instance that is ready
 // to be used as a Storer.
-func NewMemstore() *Memstore {
-	return &Memstore{
-		accounts: map[string]accounts.Account{},
+func NewMemstore() (*Memstore, error) {
+	db, err := memdb.NewMemDB(schema)
+	if err != nil {
+		return nil, err
 	}
+	return &Memstore{
+		db: db,
+	}, nil
 }
 
 // Create inserts the passed Account into the Memstore,
 // returning an ErrAccountAlreadyExists error if an Account
 // with the same ID already exists in the Memstore.
 func (m *Memstore) Create(ctx context.Context, account accounts.Account) error {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-	if _, ok := m.accounts[account.ID]; ok {
+	txn := m.db.Txn(true)
+	defer txn.Abort()
+	exists, err := txn.First("account", "id", account.ID)
+	if err != nil {
+		return err
+	}
+	if exists != nil {
 		return accounts.ErrAccountAlreadyExists
 	}
-	m.accounts[account.ID] = account
+	err = txn.Insert("account", &account)
+	if err != nil {
+		return err
+	}
+	txn.Commit()
 	return nil
 }
 
@@ -39,30 +72,36 @@ func (m *Memstore) Create(ctx context.Context, account accounts.Account) error {
 // the Memstore, returning an ErrAccountNotFound error if no
 // Account matches the passed ID.
 func (m *Memstore) Get(ctx context.Context, id string) (accounts.Account, error) {
-	m.lock.RLock()
-	defer m.lock.RUnlock()
-
-	account, ok := m.accounts[id]
-	if !ok {
+	txn := m.db.Txn(false)
+	account, err := txn.First("account", "id", id)
+	if err != nil {
+		return accounts.Account{}, err
+	}
+	if account == nil {
 		return accounts.Account{}, accounts.ErrAccountNotFound
 	}
-
-	return account, nil
+	return *account.(*accounts.Account), nil
 }
 
 // Update applies the passed Change to the Account that matches
 // the specified ID in the Memstore, if any Account matches the
 // specified ID in the Memstore.
 func (m *Memstore) Update(ctx context.Context, id string, change accounts.Change) error {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	account, ok := m.accounts[id]
-	if !ok {
+	txn := m.db.Txn(true)
+	defer txn.Abort()
+	account, err := txn.First("account", "id", id)
+	if err != nil {
+		return err
+	}
+	if account == nil {
 		return nil
 	}
-	updated := accounts.Apply(change, account)
-	m.accounts[id] = updated
+	updated := accounts.Apply(change, *account.(*accounts.Account))
+	err = txn.Insert("account", &updated)
+	if err != nil {
+		return err
+	}
+	txn.Commit()
 	return nil
 }
 
@@ -70,9 +109,19 @@ func (m *Memstore) Update(ctx context.Context, id string, change accounts.Change
 // the Memstore, if any Account matches the specified ID in the
 // Memstore.
 func (m *Memstore) Delete(ctx context.Context, id string) error {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	delete(m.accounts, id)
+	txn := m.db.Txn(true)
+	defer txn.Abort()
+	exists, err := txn.First("account", "id", id)
+	if err != nil {
+		return err
+	}
+	if exists == nil {
+		return nil
+	}
+	err = txn.Delete("account", exists)
+	if err != nil {
+		return err
+	}
+	txn.Commit()
 	return nil
 }
--- storers/memory_test.go
+++ storers/memory_test.go
@@ -13,7 +13,7 @@ func init() {
 type MemstoreFactory struct{}
 
 func (m MemstoreFactory) NewStorer(ctx context.Context) (accounts.Storer, error) {
-	return NewMemstore(), nil
+	return NewMemstore()
 }
 
 func (m MemstoreFactory) TeardownStorers() error {
